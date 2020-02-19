b18b5c596cc532e6641bfceae669ab2d2d5e4116
Eric Kretsch <55903406+Eric-Kretsch@users.noreply.github.com>
2020-02-18 22:57:16 -0600

    8 kyu Holiday VI - Shark Pontoon

--- Holiday-VI-Shark Pontoon.py
+++ Holiday-VI-Shark Pontoon.py
@@ -6,7 +6,7 @@
 
 # You are given 5 variables: sharkDistance = distance the shark needs to cover to eat you in metres, sharkSpeed = how fast it can move in metres/second, pontoonDistance = how far you need to swim to safety in metres, youSpeed = how fast you can swim in metres/second, dolphin = a boolean, if true, you can half the swimming speed of the shark as the dolphin will attack it.
 
-# If you make it, return "Alive!", if not, return "Shark Bait!".
+# If you make it, return "Alive!", if not, return "Shark Bait!". #
 
 def shark(pontoonDistance, sharkDistance, youSpeed, sharkSpeed, dolphin):
     if dolphin == True:
